d13a2b89b2b08dc5247a27a0dbb42140ae183e40
Gabriel Alves <alvesgabriel1994@gmail.com>
2025-05-10 15:06:19 -0600

    added flask version of session code

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
 import requests
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/users')
+
+@app.route("/users")
 def users():
-    response = requests.get('https://jsonplaceholder.typicode.com/users')
+    time.sleep(2)
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
     users = response.json()
-    return render_template('users.html', users=users)
+    return render_template("users.html", users=users)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
